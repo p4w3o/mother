5dcaedfff721b7759c48ed05293c7b6b24d44d90
paweo <paveovdvk@gmail.com>
2025-09-28 22:44:59 +0200

    Add motherCLI Xcode project

---
+++ motherCLI/motherCLI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  motherCLI
+//
+//  Created by paweo on 28/09/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ motherCLI/motherCLI/motherCLIApp.swift
@@ -0,0 +1,17 @@
+//
+//  motherCLIApp.swift
+//  motherCLI
+//
+//  Created by paweo on 28/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct motherCLIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
